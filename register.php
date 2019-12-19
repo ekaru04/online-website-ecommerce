eb00cd9315cc37ea6e2bddf323c870795008fe47
FranciskaWulandari <55430961+FranciskaWulandari@users.noreply.github.com>
2019-12-19 09:09:25 +0700

    title

--- register.php
+++ register.php
@@ -7,7 +7,8 @@ $o= mysqli_query($host, "SELECT * FROM tb_kategori");
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>CleanInKlik</title>
+    <title>Clean in Click</title>
+  	<link rel="icon" href="images/icon.jpg" type="images/icon.jpg">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">

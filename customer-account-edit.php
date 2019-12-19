179a27be804ee40adaa9cd36ca1a49fc8afcc1d6
viqih999 <55432475+viqih999@users.noreply.github.com>
2019-12-19 10:44:45 +0700

    update tampilan (footer)

--- customer-account-edit.php
+++ customer-account-edit.php
@@ -242,41 +242,28 @@ if($_SESSION['username'] == null){
       <div class="container">
         <div class="row">
           <div class="col-lg-3 col-md-6">
-            <h4 class="mb-3">Pages</h4>
-            <ul class="list-unstyled">
-              <li><a href="text.html">About us</a></li>
-              <li><a href="text.html">Terms and conditions</a></li>
-              <li><a href="faq.html">FAQ</a></li>
-              <li><a href="contact.html">Contact us</a></li>
-            </ul>
-            <hr>
             <h4 class="mb-3">User section</h4>
             <ul class="list-unstyled">
-              <li><a href="#" data-toggle="modal" data-target="#login-modal">Login</a></li>
+              <li><a href="" data-toggle="modal" data-target="#login-modal">Login</a></li>
               <li><a href="register.html">Regiter</a></li>
             </ul>
           </div>
           <!-- /.col-lg-3-->
           <div class="col-lg-3 col-md-6">
-            <h4 class="mb-3">Top categories</h4>
-            <h5>Men</h5>
+            <h4 class="mb-3">Contact Admin</h4>
+            <h5><strong>Franciska W</strong></h5>
             <ul class="list-unstyled">
-              <li><a href="category.html">T-shirts</a></li>
-              <li><a href="category.html">Shirts</a></li>
-              <li><a href="category.html">Accessories</a></li>
+              <li>Email</li>
             </ul>
-            <h5>Ladies</h5>
+            <h5><strong>Achmad Wildhan Eka</strong></h5>
             <ul class="list-unstyled">
-              <li><a href="category.html">T-shirts</a></li>
-              <li><a href="category.html">Skirts</a></li>
-              <li><a href="category.html">Pants</a></li>
-              <li><a href="category.html">Accessories</a></li>
+              <li><a href="mailto: wildhan.simdig04@gmail.com">wildhan.simdig04@gmail.com</a></li>
             </ul>
           </div>
           <!-- /.col-lg-3-->
           <div class="col-lg-3 col-md-6">
-            <h4 class="mb-3">Where to find us</h4>
-            <p><strong>Obaju Ltd.</strong><br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br><strong>Great Britain</strong></p><a href="contact.html">Go to contact page</a>
+            <h4 class="mb-3">Alamat Kantor</h4>
+            <p><strong>Perum Tidar Cluster</strong><br>Blok Sekian<br>Tidar<br>45Y 73J<br>Jember<br><strong>Great Britain</strong></p>
             <hr class="d-block d-md-none">
           </div>
           <!-- /.col-lg-3-->
@@ -303,6 +290,10 @@ if($_SESSION['username'] == null){
     <!-- /#footer-->
     <!-- *** FOOTER END ***-->
     
+      
+    <!-- /#footer-->
+    <!-- *** FOOTER END ***-->
+    
     
     <!--
     *** COPYRIGHT ***
@@ -312,10 +303,9 @@ if($_SESSION['username'] == null){
       <div class="container">
         <div class="row">
           <div class="col-lg-6 mb-2 mb-lg-0">
-            <p class="text-center text-lg-left">©2019 Your name goes here.</p>
+            <p class="text-center text-lg-left">©2019 Click in Click.</p>
           </div>
           <div class="col-lg-6">
-            <p class="text-center text-lg-right">Template design by <a href="https://bootstrapious.com/p/big-bootstrap-tutorial">Bootstrapious</a>
               <!-- If you want to remove this backlink, pls purchase an Attribution-free License @ https://bootstrapious.com/p/obaju-e-commerce-template. Big thanks!-->
             </p>
           </div>
---
+++ footer.php
@@ -0,0 +1,66 @@
+ 
+
+ <div id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">User section</h4>
+            <ul class="list-unstyled">
+              <li><a href="" data-toggle="modal" data-target="#login-modal">Login</a></li>
+              <li><a href="register.html">Regiter</a></li>
+            </ul>
+          </div>
+          <!-- /.col-lg-3-->
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">Contact Admin</h4>
+            <h5><strong>Franciska W</strong></h5>
+            <ul class="list-unstyled">
+              <li>Email</li>
+            </ul>
+            <h5><strong>Achmad Wildhan Eka</strong></h5>
+            <ul class="list-unstyled">
+              <li><a href="mailto: wildhan.simdig04@gmail.com">wildhan.simdig04@gmail.com</a></li>
+            </ul>
+          </div>
+          <!-- /.col-lg-3-->
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">Alamat Kantor</h4>
+            <p><strong>Perum Tidar Cluster</strong><br>Blok Sekian<br>Tidar<br>45Y 73J<br>Jember<br><strong>Great Britain</strong></p>
+            <hr class="d-block d-md-none">
+          </div>
+          <!-- /.col-lg-3-->
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">Get the news</h4>
+            <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <form>
+              <div class="input-group">
+                <input type="text" class="form-control"><span class="input-group-append">
+                  <button type="button" class="btn btn-outline-secondary">Subscribe!</button></span>
+              </div>
+              <!-- /input-group-->
+            </form>
+            <hr>
+            <h4 class="mb-3">Stay in touch</h4>
+            <p class="social"><a href="#" class="facebook external"><i class="fa fa-facebook"></i></a><a href="#" class="twitter external"><i class="fa fa-twitter"></i></a><a href="#" class="instagram external"><i class="fa fa-instagram"></i></a><a href="#" class="gplus external"><i class="fa fa-google-plus"></i></a><a href="#" class="email external"><i class="fa fa-envelope"></i></a></p>
+          </div>
+          <!-- /.col-lg-3-->
+        </div>
+        <!-- /.row-->
+      </div>
+      <!-- /.container-->
+    </div>
+    <!-- /#footer-->
+    <!-- *** FOOTER END ***-->
+    <div id="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 mb-2 mb-lg-0">
+            <p class="text-center text-lg-left">©2019 Click in Click.</p>
+          </div>
+          <div class="col-lg-6">
+              <!-- If you want to remove this backlink, pls purchase an Attribution-free License @ https://bootstrapious.com/p/obaju-e-commerce-template. Big thanks!-->
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>

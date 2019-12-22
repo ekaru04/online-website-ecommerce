d181eec2955877e30a9676715d8f4ae644330961
ShintaMauliantika <shintamauliantika59@gmail.com>
2019-12-22 17:55:29 +0700

    Shinta Mauliantika

--- boneka.php
+++ boneka.php
@@ -191,7 +191,7 @@
         <div class="container">
           <div class="col-md-12">
             <div id="blog-homepage" class="row">
-              <div class="col-sm-6">
+              <!-- <div class="col-sm-6">
                 <div class="post">
                   <h4><a href="post.html">Fashion now</a></h4>
                   <p class="author-category">By <a href="#">John Slim</a> in <a href="">Fashion and style</a></p>
@@ -199,8 +199,8 @@
                   <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
                   <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a></p>
                 </div>
-              </div>
-              <div class="col-sm-6">
+              </div> -->
+              <!-- <div class="col-sm-6">
                 <div class="post">
                   <h4><a href="post.html">Who is who - example blog post</a></h4>
                   <p class="author-category">By <a href="#">John Slim</a> in <a href="">About Minimal</a></p>
@@ -208,7 +208,7 @@
                   <p class="intro">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
                   <p class="read-more"><a href="post.html" class="btn btn-primary">Continue reading</a></p>
                 </div>
-              </div>
+              </div> -->
             </div>
             <!-- /#blog-homepage-->
           </div>

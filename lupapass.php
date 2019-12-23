3f4cdb7ced1cc081a3d4dfc91d8e028223ba7b72
FranciskaWulandari <55430961+FranciskaWulandari@users.noreply.github.com>
2019-12-23 23:18:40 +0700

    ?

--- lupapass.php
+++ lupapass.php
@@ -146,7 +146,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/Rebellion/lupapass.php/koneksi.php';
       <div class="container">
         <div class="row">
           <div class="col-lg-6 mb-2 mb-lg-0">
-            <p class="text-center text-lg-left">©2019 Your name goes here.</p>
+            <p class="text-center text-lg-left">©2019 clean in click</p>
           </div>
           <div class="col-lg-6">
             <p class="text-center text-lg-right">Template design by <a href="https://bootstrapious.com/p/big-bootstrap-tutorial">Bootstrapious</a>

b23428cbda3c0fc22e720080aab648cd7f425efd
FranciskaWulandari <55430961+FranciskaWulandari@users.noreply.github.com>
2019-12-24 19:20:58 +0700

    byebye eror

--- respass.php
+++ respass.php
@@ -1,7 +1,5 @@
 <?php
-// include koneksi di database
-include $_SERVER['DOCUMENT_ROOT'].'/Rebellion/lupapass.php/koneksi.php';
-// $o= mysqli_query($host, "SELECT * FROM tb_kategori");
+include "koneksi.php";
 ?>
 <!DOCTYPE html>
 <html>

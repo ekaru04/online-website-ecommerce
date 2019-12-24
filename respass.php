1a3bde1857c72ef078d3a7fca2b36cbf091d5b79
FranciskaWulandari <55430961+FranciskaWulandari@users.noreply.github.com>
2019-12-24 20:39:51 +0700

    8

--- respass.php
+++ respass.php
@@ -46,7 +46,7 @@ include "koneksi.php";
       <div id="top">
         <div class="container">
           <div class="row">
-            <div class="col-lg-6 offer mb-3 mb-lg-0"><a href="#" class="btn btn-success btn-sm">Offer of the day</a><a href="#" class="ml-1">Get flat 35% off on orders over $50!</a></div>
+            <div class="col-lg-6 offer mb-3 mb-lg-0"><a href="#" class="ml-1"></a></div>
             <div class="col-lg-6 text-center text-lg-right">
               <ul class="menu list-inline mb-0">
                 <li class="list-inline-item"><a href="#" data-toggle="modal" data-target="#login-modal">Login</a></li>
